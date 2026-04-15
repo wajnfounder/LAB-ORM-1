ce7be1ea6ea757d4a8dcead7a103ee3e119d608c
Wajn <wajnaq1@gmail.com>
2026-04-15 09:09:33 +0300

    basic blogger completed

--- Blogger/blog/views.py
+++ Blogger/blog/views.py
@@ -21,20 +21,3 @@ def create_post_view(request: HttpRequest):
         return redirect("blog:home")
 
     return render(request, "blog/create.html")
-
-def delete_post_view(request, id):
-    post = Blog.objects.get(id=id)
-    post.delete()
-    return redirect('blog:home')
-
-def edit_post_view(request, id):
-    post = Blog.objects.get(id=id)
-
-    if request.method == "POST":
-        post.title = request.POST.get("title")
-        post.content = request.POST.get("content")
-        post.save()
-
-        return redirect('blog:home')
-
-    return render(request, "blog/edit.html", {"post": post})
